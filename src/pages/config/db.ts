2f33519df9f86662fb87ff020c1247d591f348bb
vicentefb <chenteferrara@gmail.com>
2021-12-21 20:27:38 -0600

    added models and connection to mongo db

---
+++ src/pages/config/db.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+import mongoose from "mongoose";
+
+
+// connection function
+export const connectDB = async () => {
+    const conn = await mongoose
+      .connect(process.env.MONGO_URL as string)
+      .catch(err => console.log(err))
+    console.log("Mongoose Connection Established")
+    
+    const connection = mongoose.connection;
+    if(connection.readyState >= 1){
+        console.log("Connected to database.");
+        return conn;
+    }
+    
+  }
